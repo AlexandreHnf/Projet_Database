8d0b7ce2d6eca5c41e469aed250cf099843fdcc6
AlexandreHnf <h.alexandre99@hotmail.com>
2018-04-06 14:41:23 +0200

    database compiles well now !

--- Tables/Administrateur.sql
+++
@@ -1,9 +0,0 @@
-SET NAMES utf8;
-
-DROP TABLE IF EXISTS Administrateur;
-
-CREATE TABLE Administrateur (
-    
-    Pseudo VARCHAR(30) NOT NULL PRIMARY KEY
-
-) ENGINE=InnoDB;
--- Tables/ebay.sql
+++ Tables/ebay.sql
@@ -4,65 +4,75 @@ CREATE DATABASE Ebay; -- création de la base de données
 
 USE Ebay;
 
-SET NAMES utf8;
-
--- ============================ TABLE ADMIN ============================
-DROP TABLE IF EXISTS `Administrateur`;
-
-CREATE TABLE `Administrateur` (
-    
-    `Pseudo` VARCHAR(30) NOT NULL,
-    PRIMARY KEY (`Pseudo`)
-
-) ENGINE=InnoDB;
-
+-- SET NAMES utf8;
 
 
 -- ============================ TABLE UTILISATEUR ============================
-DROP TABLE IF EXISTS Utilisateur;
+-- DROP TABLE IF EXISTS Utilisateur;
 
 CREATE TABLE Utilisateur (
     
-    Pseudo VARCHAR(30) NOT NULL PRIMARY KEY, -- clé primaire
+    Pseudo VARCHAR(30) NOT NULL, -- clé primaire
     AdresseMail VARCHAR(320) NOT NULL, -- 320 = 64 (avant @) + 1 (@) + 255 (apres @)
     MotDePasse VARCHAR(30) NOT NULL,
-    Description_user TEXT,
-    -- pas de foreign key vers Admin car on stocke pas de "supprime"
+    Description_user TEXT, -- pas de foreign key vers Admin car on stocke pas de "supprime"
+
+    PRIMARY KEY (Pseudo)
     
 
-) ENGINE=InnoDB;
+);
+
+
+
+
+-- ============================ TABLE ADMIN ============================
+-- DROP TABLE IF EXISTS Administrateur;
+
+CREATE TABLE Administrateur (
+    
+    Pseudo VARCHAR(30) NOT NULL,
+    PseudoUser VARCHAR(30) NOT NULL, -- foreign key pour l'héritage
+    PRIMARY KEY (Pseudo),
+    
+    CONSTRAINT fk_admin_user
+        FOREIGN KEY (PseudoUser)         
+        REFERENCES Utilisateur(Pseudo)
+
+);
 
 
 
 -- ============================ TABLE VENDEUR ============================
-DROP TABLE IF EXISTS Vendeur;
+-- DROP TABLE IF EXISTS Vendeur;
 
 CREATE TABLE Vendeur (
     
-    Pseudo VARCHAR(30) NOT NULL PRIMARY KEY, -- clé primaire
+    Pseudo VARCHAR(30) NOT NULL, -- clé primaire
     Nom CHAR(30) NOT NULL,
     Prenom CHAR(30) NOT NULL,
     DateNaissance DATE NOT NULL,
-    Adresse TEXT NOT NULL
-    PseudoUser VARCHAR(30) NOT NULL, -- foreign key
+    Adresse TEXT NOT NULL,
+    PseudoUser VARCHAR(30) NOT NULL, -- foreign key pour l'héritage
 
     -- CONTRAINTES D'INTEGRITE
 
-    CONSTRAINT fk_user                       -- On donne un nom à notre clé
+    PRIMARY KEY (Pseudo),
+
+    CONSTRAINT fk_vendeur_user               -- On donne un nom à notre clé
         FOREIGN KEY (PseudoUser)             -- Colonne sur laquelle on crée la clé
         REFERENCES Utilisateur(Pseudo)       -- Colonne de référence
 
-) ENGINE=InnoDB;
+);
 
 
 
 
 -- ============================ TABLE OBJET ============================
-DROP TABLE IF EXISTS Objet;
+-- DROP TABLE IF EXISTS Objet;
 
 CREATE TABLE Objet (
     
-    Titre CHAR(30) NOT NULL PRIMARY KEY, -- clé primaire
+    Titre CHAR(30) NOT NULL, -- clé primaire
     Description_obj TEXT,
     DateMiseEnVente DATE,
     PrixMin DECIMAL(6,2) NOT NULL,
@@ -73,89 +83,93 @@ CREATE TABLE Objet (
     -- On stocke plus AdminPseudo du coup vu qu'on sauve pas les supprime
 
     -- CONTRAINTES D'INTEGRITE
-
-    CONSTRAINT fk_vendeur                 -- On donne un nom à notre clé
+    PRIMARY KEY (Titre),
+    
+    CONSTRAINT fk_obj_vendeur             -- On donne un nom à notre clé
         FOREIGN KEY (PseudoVendeur)       -- Colonne sur laquelle on crée la clé
         REFERENCES Vendeur(Pseudo)        -- Colonne de référence
 
-
-) ENGINE=InnoDB;
+);
 
 
 
 -- ============================ TABLE EVALUATION ============================
-DROP TABLE IF EXISTS Evaluation;
+-- DROP TABLE IF EXISTS Evaluation;
 
 CREATE TABLE Evaluation (
     
     Numero INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, -- clé primaire
     Note SMALLINT NOT NULL,
     Date_eval DATETIME NOT NULL,
-    Commentaire TEXT 
+    Commentaire TEXT,
     TitreObj CHAR(30) NOT NULL, -- foreign key
     PseudoVendeur VARCHAR(30) NOT NULL, -- foreign key
     PseudoUser VARCHAR(30) NOT NULL, -- foreign key
 
     -- CONTRAINTES D'INTEGRITE
 
-    CONSTRAINT fk_objet                  -- On donne un nom à notre clé
-        FOREIGN KEY (TitreObj)           -- Colonne sur laquelle on crée la clé
-        REFERENCES Objet(Titre)          -- Colonne de référence
+    CONSTRAINT fk_obj_eval                -- On donne un nom à notre clé
+        FOREIGN KEY (TitreObj)            -- Colonne sur laquelle on crée la clé
+        REFERENCES Objet(Titre),          -- Colonne de référence
 
-    CONSTRAINT fk_vendeur                
+    CONSTRAINT fk_eval_vendeur                
         FOREIGN KEY (PseudoVendeur)      
-        REFERENCES Vendeur(Pseudo)       
+        REFERENCES Vendeur(Pseudo),       
 
-    CONSTRAINT fk_utilisateur            
+    CONSTRAINT fk_eval_user            
         FOREIGN KEY (PseudoUser)         
         REFERENCES Utilisateur(Pseudo)   
 
 
-) ENGINE=InnoDB;
+);
 
 
 
 -- ============================ TABLE CATEGORIE ============================
-DROP TABLE IF EXISTS Categorie;
+-- DROP TABLE IF EXISTS Categorie;
 
 CREATE TABLE Categorie (
     
-    Titre VARCHAR(30) NOT NULL PRIMARY KEY, -- clé primaire
-    Description_cat TEXT
+    Titre VARCHAR(30) NOT NULL, -- clé primaire
+    Description_cat TEXT,
     PseudoAdmin VARCHAR(30) NOT NULL, -- foreign key
 
     -- CONTRAINTES D'INTEGRITE
 
-    CONSTRAINT fk_admin             
+    PRIMARY KEY (Titre),
+
+    CONSTRAINT fk_admin_cat           
         FOREIGN KEY (PseudoAdmin)         
         REFERENCES Administrateur(Pseudo)
     
 
-) ENGINE=InnoDB;
+);
 
 
 
 -- ============================ TABLE PROP ACHAT ============================
-DROP TABLE IF EXISTS PropositionAchat;
+-- DROP TABLE IF EXISTS PropositionAchat;
 
 CREATE TABLE PropositionAchat (
     
-    TitreObjet CHAR(30) NOT NULL PRIMARY KEY, -- clé primaire
+    TitreObjet CHAR(30) NOT NULL, -- clé primaire
     AcheteurPotentiel VARCHAR(30),
     PrixPropose DECIMAL(6,2) NOT NULL,
-    Etat TINYINT(1) NOT NULL
+    Etat TINYINT(1) NOT NULL,
     TitreObj CHAR(30) NOT NULL, -- foreign key
     PseudoUser VARCHAR(30) NOT NULL, -- foreign key
     
-    CONSTRAINT fk_objet            
-        FOREIGN KEY (TitreObj)         
-        REFERENCES Objet(Titre)
+    PRIMARY KEY (TitreObjet),
 
-    CONSTRAINT fk_user            
+    CONSTRAINT fk_obj_prop          
+        FOREIGN KEY (TitreObj)         
+        REFERENCES Objet(Titre),
+
+    CONSTRAINT fk_user_prop          
         FOREIGN KEY (PseudoUser)         
         REFERENCES Utilisateur(Pseudo)
 
-) ENGINE=InnoDB;
+);
 
 
 
