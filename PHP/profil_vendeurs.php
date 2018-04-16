c39b45ae45fc0425216189295a836e8d4cb302fe
AlexandreHnf <h.alexandre99@hotmail.com>
2018-04-16 21:01:02 +0200

    seller profile done and working :)

--- PHP/profil_vendeurs.php
+++ PHP/profil_vendeurs.php
@@ -7,11 +7,11 @@
 <html>
    <head>
        <meta charset="utf-8" />
-       <title>Vendre</title>
+       <title>Profil des vendeurs</title>
    </head>
 
    <body>
-        <h2> Vendre </h2> 
+        <h2> Profil vendeurs </h2> 
 
         <?php
 
@@ -31,10 +31,10 @@
             echo "Date de naissance : " . $profil['DateNaissance'] . "<br>";
             echo "Adresse : " . $profil["Adresse"] . "<br>";
             echo "Adresse mail : " . $profil["AdresseMail"] . "<br>";
-            echo "Description : " . $profil["Description_user"];
+            echo "Description : " . $profil["Description_user"] . "<br><br>";
 
             // affiche du lien pour retour
-            echo '<a href="profil_vendeurs?page=' . $_GET['page'] . '">' . Retour . '</a> ';
+            echo '<a href="profil_vendeurs.php?page=' . $_GET['page'] . '">' . "Retour" . '</a> ';
         }
 
         else{ // On affiche la liste des vendeurs
@@ -42,37 +42,44 @@
             if (isset($_GET['page'])) {
                 $page = $_GET['page'];
             }
-            else { $page = 1; }
+            else { 
+                $page = 1; // par défaut
+            }
 
 
             // On met dans une variable le nombre de messages qu'on veut par page
-            $nombreDeMessagesParPage = 100; // Nombre d'éléments par page
+            $nb_mess_per_page = 100; // Nombre d'éléments par page
             // On récupère le nombre total de messages
             $req = $bdd->query('SELECT COUNT(*) AS nb_messages FROM Vendeur');
             $donnees = $req->fetch();
             $total = $donnees ['nb_messages'];
 
             // On calcule le nombre de pages à créer
-            $nombreDePages  = ceil($totalDesMessages / $nombreDeMessagesParPage);
+            $nombreDePages  = ceil($total / $nb_mess_per_page);
             
             // Puis on fait une boucle pour écrire les liens vers chacune des pages
-            echo 'Page : ';
+            echo 'Pages : ';
             for ($i = 1 ; $i <= $nombreDePages ; $i++) {
-                echo '<a href="profil_vendeurs?page=' . $i . '">' . $i . '</a> ';
+                echo '<a href="profil_vendeurs.php?page=' . $i . '">' . $i . '</a> ';
             }
+            echo "<br><br>";
 
             // On calcule le numéro du premier message qu'on prend pour le LIMIT
-            $premierMessageAafficher = ($page - 1) * $nombreDeMessagesParPage;
+            $premierMessageAafficher = ($page - 1) * $nb_mess_per_page;
 
-            $req1 = $bdd->prepare('SELECT SellerID, Nom, Prenom FROM Vendeur 
-                                    ORDER BY SellerID DESC LIMIT ' . ? . ', ' . ?);
-            $req1->execute(array($premierMessageAafficher, $nombreDeMessagesParPage));
-            while ($donnees = $req1->fetch()) {
+            $req1 = $bdd->query('SELECT SellerID, Nom, Prenom FROM Vendeur             
+                    ORDER BY SellerID 
+                    DESC LIMIT ' . $premierMessageAafficher . ', ' . $nb_mess_per_page . '');
+
+            while ($seller = $req1->fetch()) {
+                $id = $seller['SellerID'];
                 // print des liens
-                echo '<a href="profil_vendeurs?page=' . $page . '">' 
-                    . $donnees['Nom'] . " " . $donnees['Prenom'] . '</a> ';
+                echo '<a href="profil_vendeurs.php?page=' . $page . '&SellerID=' . $id . '">' 
+                    . $seller['Nom'] . " " . $seller['Prenom'] . '</a> ' . "<br>";
             }
         }
+
+        ?>
 
    </body>
 </html>
