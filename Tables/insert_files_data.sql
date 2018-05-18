346431b508de634d86a5c07271a761df4e2c0312
root <nicoj01@scarlet.be>
2018-05-18 19:19:22 +0200

    Cleaned doublons .sql files

--- Tables/insert_files_data.sql
+++ Tables/insert_files_data.sql
@@ -1,3 +1,7 @@
+INSERT INTO Utilisateur
+VALUES (@ignore, SHA1('master_admin'), 'Master', 'master0@hotmail.com',
+        'Administrateur suprême');
+
 -- =========================== USERS.TXT > UTILISATEUR ===================
 
 LOAD DATA LOCAL INFILE '/opt/lampp/phpmyadmin/data/dataset_ebay_v2/users.txt'
@@ -6,7 +10,6 @@ FIELDS TERMINATED BY ', '
 LINES TERMINATED BY '\n' 
 (UserID, MotDePasse, Pseudo, AdresseMail)
 set MotDePasse = SHA1(MotDePasse);
-
 -- =========================== SELLERS.TXT > UTILISATEUR ===================
 
 LOAD DATA LOCAL INFILE '/opt/lampp/phpmyadmin/data/dataset_ebay_v2/sellers.txt'
@@ -14,7 +17,13 @@ INTO TABLE Utilisateur
 FIELDS TERMINATED BY ', '
 LINES TERMINATED BY '\n' 
 (UserID, @ignore, @ignore, Pseudo, MotDePasse, @ignore, @ignore, AdresseMail)
-set MotDePasse = MotDePasse;
+set MotDePasse = SHA1(MotDePasse);
+
+-- ========================== INSERTION ADMINISTRATEUR MASTER ================
+
+INSERT INTO Administrateur
+VALUES (1);
+
 
 -- =========================== SELLERS.TXT > VENDEUR ===================
 
@@ -25,6 +34,12 @@ LINES TERMINATED BY '\n'
 (SellerID, Prenom, Nom, @ignore, @ignore, @date, Adresse, @ignore)
 SET DateNaissance = STR_TO_DATE(@date, '%d-%m-%Y ');
 
+
+-- ======================Ajout de la Categorie par défaut Default===========
+INSERT INTO Categorie
+VALUES ('Default', 'Catégorie par défaut', NULL);
+
+
 -- =========================== ITEMS.TXT > OBJET ===================
 
 LOAD DATA LOCAL INFILE '/opt/lampp/phpmyadmin/data/dataset_ebay_v2/items.txt'
@@ -34,16 +49,16 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (ItemID, SellerID, Titre, Description_obj, Categorie, PrixMin, DateMiseEnVente);
 
+
 -- ============================== REVIEWS.XML > EVALUATION ==================
 
 LOAD XML LOCAL INFILE '/opt/lampp/phpmyadmin/data/dataset_ebay_v2/reviews.xml' 
 INTO TABLE Evaluation
 ROWS IDENTIFIED BY '<Review>';
 
+
 -- ================ PROPOSITION.XML > PROPOSITIONACHAT=====================
 
 LOAD XML LOCAL INFILE '/opt/lampp/phpmyadmin/data/dataset_ebay_v2/propositions.xml' 
 INTO TABLE PropositionAchat
 ROWS IDENTIFIED BY '<Proposition>';
-
-
